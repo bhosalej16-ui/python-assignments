ac1b4b1929f4f8d093dcb566fc1a11f9656e07bb
bhosalej16-ui <bhosalej16@gmail.com>
2026-04-18 14:40:39 +0530

    Add files via upload

---
+++ program 6 (jason file).py
@@ -0,0 +1,7 @@
+[
+    {"name":"jay","mark":"98","age":19},
+     {"name":"sarth","mark":"90","age":19},
+      {"name":"blaze","mark":"80","age":18},
+       {"name":"shon","mark":"79","age":19}
+    
+]
